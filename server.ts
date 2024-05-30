7cb3ba361604dfd67e3a65fb487e406f7f309b16
function09 <houssine199222@gmail.com>
2024-05-30 00:07:02 -0400

    Initialize tsconfig and rework server.ts

---
+++ server.ts
@@ -0,0 +1,7 @@
+import app from "./app/index.js";
+
+const port: string = process.env.PORT || "3000";
+
+app.listen(port, ():void =>  {
+    console.log(`Server is listening on port: ${port}`);
+});
